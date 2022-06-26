664ee2a7a5f9705a2fbbc4b8ce7ec56987a94303
NadezhdaM <nadia_07.87@inbox.ru>
2022-06-26 16:16:58 +0300

    1 and 2 done

---
+++ HomeWorkSeminar1/Program.cs
@@ -0,0 +1,69 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.//
+/*
+int a;
+int b;
+
+
+Console. WriteLine("Input number a: ");
+a=Convert.ToInt32(Console.ReadLine());
+
+Console. WriteLine("Input number b: ");
+b=Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+     Console.WriteLine("the numbers are equal");
+}
+
+else
+
+  if (a > b) 
+  {
+   Console.WriteLine("max" + a); 
+   Console.WriteLine("min" + b);
+  }
+ 
+  else
+  {
+   Console.WriteLine("max" + b); 
+   Console.WriteLine("min" + a);
+  } 
+
+*/
+
+
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.//
+/*
+int a;
+int b;
+int c;
+
+Console. WriteLine("Input number a: ");
+a=Convert.ToInt32(Console.ReadLine());
+
+Console. WriteLine("Input number b: ");
+b=Convert.ToInt32(Console.ReadLine());
+
+Console. WriteLine("Input number c: ");
+c=Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+   if (a > c)
+    {
+     Console.WriteLine("max" + a);
+    }
+    else
+         Console.WriteLine("max" + c);
+else
+     if (b > c)
+     {
+      Console.WriteLine("max" + b);    
+     }
+     else
+       Console.WriteLine("max" + c);    
+     
+*/
+
+
+
