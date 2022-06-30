eb2fe76c2779d8081b0a3d7bf8e82dba88047fd9
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-01 02:40:22 +0300

    1

---
+++ HomeWorkSeminar2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа//
+
+int CutNumber()
+{
+Console. WriteLine("Input number from 100 to 999: ");
+num=Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+}
