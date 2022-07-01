fdff47896c2c9deba97e1aeccb3ff2a666422cd6
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-01 15:52:23 +0300

    1 and 3 done

--- HomeWorkSeminar2/Program.cs
+++ HomeWorkSeminar2/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа//
-
+/*
 int CutNumber()
 {
 int num;
@@ -19,3 +19,70 @@ return result;
  int num = CutNumber();
  Console.WriteLine(num);
 
+*/
+
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.//
+/*
+int CutNumber()
+{
+int num;
+
+Console. WriteLine("Input number : ");
+num = Convert.ToInt32(Console.ReadLine());
+
+
+}
+
+*/
+
+
+
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным//
+/*
+int Weekend()
+
+{
+int day;
+
+Console. WriteLine("Input number from 1 to 7: ");
+day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1)
+Console.WriteLine("Monday");
+
+if (day == 2)
+Console.WriteLine("Tuesday");
+
+if (day == 3)
+Console.WriteLine("Wednesday");
+    
+if (day == 4)
+Console.WriteLine("Thursday");
+
+if (day == 5)
+Console.WriteLine("Friday");
+
+if (day == 6)
+Console.WriteLine("Saturday");
+
+if (day == 7)
+Console.WriteLine("Sunday");
+
+    if (day == 6 || day == 7)
+    Console. WriteLine("It is a weekend");
+
+    if (day > 0 && day < 6)
+    Console. WriteLine("it's a work day, sorry");
+
+       if (day < 1 || day > 7)
+       Console. WriteLine("you entered the wrong number");
+
+return day;
+    
+}
+
+int day = Weekend();
+Console.WriteLine();
+
+*/
