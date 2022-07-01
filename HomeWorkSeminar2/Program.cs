e49e9ed4ea6e775ef68dead0331396692f6787e0
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-01 03:52:02 +0300

    1 done

--- HomeWorkSeminar2/Program.cs
+++ HomeWorkSeminar2/Program.cs
@@ -2,11 +2,20 @@
 
 int CutNumber()
 {
+int num;
+
 Console. WriteLine("Input number from 100 to 999: ");
-num=Convert.ToInt32(Console.ReadLine());
+num = Convert.ToInt32(Console.ReadLine());
 
+int ten = num / 10;
 
+int result = ten % 10;
 
-
-
+return result;
 }
+
+
+
+ int num = CutNumber();
+ Console.WriteLine(num);
+
