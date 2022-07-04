b63a74606d56c1425b3ecac56562bf1a7a0d3c5f
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-04 13:26:05 +0300

    done 2

---
+++ HomeWorkSeminar3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N//
+/*
+void Cube(int n)
+{
+  
+int current = 1;
+
+while (n >= current) 
+{ int cube = current*current*current;
+  Console.Write(cube + "   ");
+  current++;    
+}
+
+}
+
+
+
+Console.WriteLine("Input num");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Cube(n);
+*/
