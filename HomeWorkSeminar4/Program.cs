688f1bb1af1bcff1466ffcdcf5d2ad768d528c5f
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-07 01:27:57 +0300

    1 done

---
+++ HomeWorkSeminar4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе//
+/*
+int Sum(int n)
+{
+
+int sum = 0;
+
+while (n > 0)
+{
+    int current = n % 10;
+    n = n / 10;
+   sum = sum + current;
+}
+
+return sum;
+
+}
+
+
+Console.WriteLine("Input n:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Sum(n));
+*/
