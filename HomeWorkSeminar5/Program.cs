1dce65ef813adb753e8ef9ec7a49741cc7376548
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-11 01:19:20 +0300

    HW5 is 2 done d

--- HomeWorkSeminar5/Program.cs
+++ HomeWorkSeminar5/Program.cs
@@ -23,6 +23,26 @@ return newArray;
 
 }
 
+/*int [] Double (int size)
+{
+    int [] newArray = new int [size];
+
+    for (int i= 0; i < size; i++)
+    newArray[i] = new Random().Next();
+
+return newArray;
+
+
+}
+
+*/
+
+
+
+
+
+
+
 void ShowArray (int [] array)
 {
    for (int i = 0; i < array.Length; i++)
@@ -77,3 +97,6 @@ ShowArray(array);
 
 Console.WriteLine("Sum of elements in odd positions is:   " + Sum(array)); 
 */
+
+
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.//
