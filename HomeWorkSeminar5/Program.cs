6f3a88609cb32aef1d08269e77342207c89f639d
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-11 00:25:36 +0300

    HW5 is 2 done

--- HomeWorkSeminar5/Program.cs
+++ HomeWorkSeminar5/Program.cs
@@ -1,12 +1,22 @@
-﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.//
-
-
+﻿
 int [] RandomArray (int size)
 {
     int [] newArray = new int [size];
 
     for (int i= 0; i < size; i++)
     newArray[i] = new Random().Next(100, 1000);
+
+return newArray;
+
+
+}
+
+int [] RandomArray1 (int size)
+{
+    int [] newArray = new int [size];
+
+    for (int i= 0; i < size; i++)
+    newArray[i] = new Random().Next();
 
 return newArray;
 
@@ -33,7 +43,21 @@ int Count (int [] newArray)
 return count;
 }   
 
+int Sum (int [] newArray)
+{
+   int sum = 0;
 
+   for (int i = 0; i < newArray.Length; i++)
+        if (i % 2 != 0)
+        sum = sum + newArray[i]; //здесь рассматриваются НЕЧЕТНЫЕ для программы; если надо НЕЧЕТНЫЕ с тч пользователя, то надо сменить условие нечетности на условие четности//
+   
+return sum;
+
+}
+
+// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.//
+
+/*
 Console.WriteLine("Input size of array:   ");
 int size = Convert.ToInt32(Console.ReadLine());
 
@@ -41,3 +65,15 @@ int [] array = RandomArray(size);
 ShowArray(array);
 
 Console.WriteLine("Count of even-numbered is:   " + Count(array));
+*/
+
+//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.//
+/*
+Console.WriteLine("Input size of array:   ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int [] array = RandomArray(size);
+ShowArray(array);
+
+Console.WriteLine("Sum of elements in odd positions is:   " + Sum(array)); 
+*/
