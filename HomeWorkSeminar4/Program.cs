5ebafd73537fd68052048e11fe09404b490f57c8
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-10 22:33:52 +0300

    1234

--- HomeWorkSeminar4/Program.cs
+++ HomeWorkSeminar4/Program.cs
@@ -28,16 +28,39 @@ Console.WriteLine(Sum(n));
 // Напишите программу, которая задаёт массив из m элементов и выводит их на экран.//
 
 
-void ShowArray(int [] array)
+int [] RandomArray (int size, int min, int max)
 {
-  for (int i = 0; i < array.Length; i++)
-  Console.Write(array[i] + "  ");
-  Console.WriteLine();
+    int [] newArray = new int [size];
+
+    for (int i= 0; i < size; i++)
+    newArray[i] = new Random().Next(min, max+1);
+
+return newArray;
+
 
 }
 
-Console.Write ("Input array size:   ");
-int size = Convert.ToInt32(Console.ReadLine());
+void ShowArray (int [] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   Console.Write(array [i]+ "   ");
 
-ShowArray(size);
+   Console.WriteLine();
+
+}
+    
+Console.WriteLine("Input size of array:   ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input min element of array:   ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input max element of arra:   ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int [] array = RandomArray(m, min, max);
+ShowArray(array);
+
+
 
