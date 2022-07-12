b65cfcd7f01dd73106ae44410d7d941e5364debe
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-12 22:22:59 +0300

    HomeWorkSeminar5 done

--- HomeWorkSeminar5/Program.cs
+++ HomeWorkSeminar5/Program.cs
@@ -4,14 +4,14 @@ int [] RandomArray (int size)
     int [] newArray = new int [size];
 
     for (int i= 0; i < size; i++)
-    newArray[i] = new Random().Next(100, 1000);
+    newArray[i] = new Random().Next(99, 1000);
 
 return newArray;
 
 
 }
 
-int [] RandomArray1 (int size)
+int [] RandomArray1 (int size)  //Не используется в выполнении ДЗ, так как генерирует сумашедшие числа, но по условию 2 задачи - надо сделать//
 {
     int [] newArray = new int [size];
 
@@ -23,25 +23,16 @@ return newArray;
 
 }
 
-/*int [] Double (int size)
+double [] Double (int size)
 {
-    int [] newArray = new int [size];
+    double [] newArray = new double [size];
 
     for (int i= 0; i < size; i++)
-    newArray[i] = new Random().Next();
+    newArray[i] = new Random().NextDouble();
 
 return newArray;
 
-
 }
-
-*/
-
-
-
-
-
-
 
 void ShowArray (int [] array)
 {
@@ -51,6 +42,16 @@ void ShowArray (int [] array)
    Console.WriteLine();
 
 }
+
+void ShowDoubleArray (double [] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   Console.Write(array [i]+ "   ");
+
+   Console.WriteLine();
+
+}
+
 
 int Count (int [] newArray)
 {
@@ -69,13 +70,35 @@ int Sum (int [] newArray)
 
    for (int i = 0; i < newArray.Length; i++)
         if (i % 2 != 0)
-        sum = sum + newArray[i]; //здесь рассматриваются НЕЧЕТНЫЕ для программы; если надо НЕЧЕТНЫЕ с тч пользователя, то надо сменить условие нечетности на условие четности//
+        sum = sum + newArray[i]; //здесь рассматриваются НЕЧЕТНЫЕ для программы; если надо НЕЧЕТНЫЕ с тз пользователя, то надо сменить условие нечетности на условие четности//
    
 return sum;
 
 }
 
-// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.//
+double Difference (double [] newArray)
+{
+
+double max = newArray[0];
+double min = newArray[0];
+
+   for (int i = 1; i < newArray.Length; i++) 
+
+   {
+         if (max < newArray[i])
+         max = newArray[i];
+    
+        if (min > newArray[i])
+        min = newArray[i];
+    }
+   
+double diff = max - min;
+
+return diff;
+
+}
+
+// 1. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.//
 
 /*
 Console.WriteLine("Input size of array:   ");
@@ -87,7 +110,7 @@ ShowArray(array);
 Console.WriteLine("Count of even-numbered is:   " + Count(array));
 */
 
-//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.//
+//2. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.//
 /*
 Console.WriteLine("Input size of array:   ");
 int size = Convert.ToInt32(Console.ReadLine());
@@ -99,4 +122,12 @@ Console.WriteLine("Sum of elements in odd positions is:   " + Sum(array));
 */
 
 
-// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.//
+// 3. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.//
+
+Console.WriteLine("Input size of array:   ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+double [] array = Double(size);
+ShowDoubleArray(array);
+
+Console.WriteLine("The difference between the maximum and minimum element is:  " + Difference(array));
