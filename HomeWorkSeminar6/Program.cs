2ca3cb2df52fbb8d0a713583128b3d326b16d62d
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-12 22:48:48 +0300

    Задача 1 не решена. Нужно ввод от пользователя, а не рандом

---
+++ HomeWorkSeminar6/Program.cs
@@ -0,0 +1,44 @@
+﻿
+int [] RandomArray (int size)
+{
+    int [] newArray = new int [size];
+
+    for (int i= 0; i < size; i++)
+    newArray[i] = new Random().Next(-1000, 1000);
+
+return newArray;
+
+}
+
+void ShowArray (int [] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   Console.Write(array [i]+ "   ");
+
+   Console.WriteLine();
+
+}
+
+int PositiveNumb (int [] newArray)
+{
+    int count = 0;
+
+   for (int i = 0; i < newArray.Length; i++)
+        if (newArray[i] > 0)
+        count ++;
+   
+return count;
+}   
+
+
+
+
+//1. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.//
+
+Console.WriteLine("Input m:   ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int [] array = RandomArray(m);
+ShowArray(array);
+
+Console.WriteLine("total positive numbers is:   " + PositiveNumb(array));
