7293e5ce1bd7aa428bb68b5a6deab54b59917de6
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-17 17:16:24 +0300

    done 1 t

--- HomeWorkSeminar6/Program.cs
+++ HomeWorkSeminar6/Program.cs
@@ -1,44 +1,36 @@
 ﻿
-int [] RandomArray (int size)
-{
-    int [] newArray = new int [size];
-
-    for (int i= 0; i < size; i++)
-    newArray[i] = new Random().Next(-1000, 1000);
-
-return newArray;
-
-}
-
-void ShowArray (int [] array)
-{
-   for (int i = 0; i < array.Length; i++)
-   Console.Write(array [i]+ "   ");
-
-   Console.WriteLine();
-
-}
-
-int PositiveNumb (int [] newArray)
-{
-    int count = 0;
-
-   for (int i = 0; i < newArray.Length; i++)
-        if (newArray[i] > 0)
-        count ++;
-   
-return count;
-}   
-
-
-
-
-//1. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.//
-
-Console.WriteLine("Input m:   ");
+ //Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.//
+ 
+Console.Write($"Input М (size of array): ");
 int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
 
-int [] array = RandomArray(m);
-ShowArray(array);
 
-Console.WriteLine("total positive numbers is:   " + PositiveNumb(array));
+void InputNumbers(int m)
+{
+
+   for (int i = 0; i < m; i++)
+     {
+       Console.Write($"Input {i+1} number: ");
+       array[i] = Convert.ToInt32(Console.ReadLine());
+     }
+}
+ 
+
+int Total(int[] array)
+{
+int count = 0;
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    if(array[i] > 0 ) count = count + 1; 
+  }
+
+return count;
+}
+
+
+InputNumbers(m);
+
+Console.WriteLine("Total natural numbers is:  " + Total(array));
+
