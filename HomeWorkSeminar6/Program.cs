bf55ea2d76401e46d3d30dd99d572b8b8a511688
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-18 16:11:34 +0300

    ka

--- HomeWorkSeminar6/Program.cs
+++ HomeWorkSeminar6/Program.cs
@@ -39,69 +39,6 @@ Console.WriteLine("Total natural numbers is:  " + Total(array));
 /*Напишите программу, которая найдёт точку пересечения двух прямых, 
 заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем*/
 
-/*double[] crossPoint = new double[2];
-double[,] coeff = new double[2, 2];
-
-void InputCoefficients()
-{
-  
-  for (int i = 0; i < coeff.GetLength(0); i++)
-  {
-    Console.Write($"Введите коэффициенты {i+1}-го уравнения (y = k * x + b):\n");
-    for (int j = 0; j < coeff.GetLength(1); j++)
-    {
-      if(j==0) Console.Write($"Введите коэффициент k: ");
-      else Console.Write($"Введите коэффициент b: ");
-      coeff[i,j] = Convert.ToInt32(Console.ReadLine());
-    }
-  }
-}
-
-double[] Decision(double[,] coeff)
-{
-  crossPoint[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
-  crossPoint[1] = crossPoint[0] * coeff[0,0] + coeff[0,1];
-  return crossPoint;
-}
-
-void OutputResponse(double[,] coeff)
-{
-  if (coeff[0,0] == coeff[1,0] && coeff[0,1] == coeff[1,1]) 
-  {
-    Console.Write("Прямые совпадают");
-  }
-  else if (coeff[0,0] == coeff[1,0] && coeff[0,1] != coeff[1,1]) 
-  {
-    Console.Write("Прямые параллельны");
-  }
-  else 
-  {
-    Decision(coeff);
-    Console.Write($"\nТочка пересечения прямых: ({crossPoint[0]}, {crossPoint[1]})");
-  }
-}
-
-InputCoefficients();
-OutputResponse(coeff);
-*/
-
-double [] Double (double x, double y)
-{
-    double [] newArray = new double [2];
-  
-
-return newArray;
-}
-
-void ShowDoubleArray (double [] array)
-{
-   for (int i = 0; i < array.Length; i++)
-   Console.Write(array [i]+ "   ");
-
-   Console.WriteLine();
-
-}
-
 
 Console.Write("Input k1:   ");
 double k1 = Convert.ToInt32(Console.ReadLine());
@@ -115,18 +52,25 @@ double k2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input b2:   ");
 double b2 = Convert.ToInt32(Console.ReadLine());
 
-double x = (b2 -b1)/(k1 - k2);
-double y = k1*x + b1;
 
 if ((k1 == k2) && (b1 == b2))
       Console.WriteLine("Прямые совпадают");
-
-else if (k1==k2)
-    Console.WriteLine("Прямые параллельны");
-
+else if (k1 == k2)
+    Console.WriteLine("Прямые паралельные");
 else
-Console.Write(x);
-Console.Write(y);
+Console.WriteLine((b2-b1)/(k1-k2));
+Console.WriteLine(k1*((b2-b1)/(k1-k2))+b1);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
