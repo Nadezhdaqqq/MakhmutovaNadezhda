65534255b0241012c9f1d0561dcdd53770ad9f1e
NadezhdaM <nadia_07.87@inbox.ru>
2022-07-18 16:24:01 +0300

    done

--- HomeWorkSeminar6/Program.cs
+++ HomeWorkSeminar6/Program.cs
@@ -1,11 +1,11 @@
 ﻿
  //Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.//
- /*
+ 
 Console.Write($"Input М (size of array): ");
 int m = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[m];
-*/
-/*
+
+
 void InputNumbers(int m)
 {
 
@@ -15,8 +15,8 @@ void InputNumbers(int m)
        array[i] = Convert.ToInt32(Console.ReadLine());
      }
 }
- */
-/*
+ 
+
 int Total(int[] array)
 {
 int count = 0;
@@ -28,18 +28,19 @@ int count = 0;
 
 return count;
 }
-*/
-/*
+
+
 InputNumbers(m);
 
 Console.WriteLine("Total natural numbers is:  " + Total(array));
-*/
+
 
 
 /*Напишите программу, которая найдёт точку пересечения двух прямых, 
 заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем*/
 
-
+void Point()
+{
 Console.Write("Input k1:   ");
 double k1 = Convert.ToInt32(Console.ReadLine());
 
@@ -54,15 +55,14 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 
 
 if ((k1 == k2) && (b1 == b2))
-      Console.WriteLine("Прямые совпадают");
-else if (k1 == k2)
-    Console.WriteLine("Прямые паралельные");
-else
-Console.WriteLine((b2-b1)/(k1-k2));
-Console.WriteLine(k1*((b2-b1)/(k1-k2))+b1);
+    Console.WriteLine("Прямые совпадают");
+        else if (k1 == k2)
+           Console.WriteLine("Прямые паралельные");
+              else
+                Console.WriteLine("Координаты точки пересечения прямых: " + (b2-b1)/(k1-k2) + (k1*((b2-b1)/(k1-k2))+b1));
+}
 
-
-
+/*Point();*/
 
 
 
